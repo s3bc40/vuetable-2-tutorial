d782e90eadc594e8033521163f9f7e2c3d19087b
s3bc40 <32032033+s3bc40@users.noreply.github.com>
2020-11-10 13:46:09 +0100

    first commit

---
+++ src/components/MyVuetable.vue
@@ -0,0 +1,35 @@
+<template>
+    <vuetable ref="vuetable"
+        api-url="https://vuetable.ratiw.net/api/users"
+        :fields="fields"
+    ></vuetable>
+</template>
+
+<script>
+import Vuetable from 'vuetable-2/src/components/Vuetable'
+
+export default {
+    components: {
+        Vuetable
+    },
+    data () {
+        return {
+            fields: [
+                'name', 'email', 'birthdate',
+                {
+                    name: 'address.line1',
+                    title: 'Address 1' 
+                },
+                {
+                    name: 'address.line2',
+                    title: 'Address 2'
+                },
+                {
+                    name: 'address.zipcode',
+                    title: 'ZipCode'
+                },
+            ]
+        }
+    }
+}
+</script>
